9ddf820d5fc18823dfe0d5b0fbc0d6b2f1a1a4e8
Michael Nascimento <michael.nsc@outlook.com>
2023-06-11 17:27:00 -0300

    ajustes de estilos

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,22 +3,24 @@ import styles from "./Navbar.module.css";
 
 function Navbar() {
   return (
-    <div className={styles.container}>
-      <h1 className={styles.logo}>MinhaListaDeCompras.com.br</h1>
-      <div>
-        <nav className={styles.navigation}>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/quem-somos">Quem somos</Link>
-            </li>
-            <li>
-              <Link to="#">Contato</Link>
-            </li>
-          </ul>
-        </nav>
+    <div className={styles.bar}>
+      <div className={styles.container}>
+        <h1 className={styles.logo}>MinhaListaDeCompras.com.br</h1>
+        <div>
+          <nav className={styles.navigation}>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/quem-somos">Quem somos</Link>
+              </li>
+              <li>
+                <Link to="#">Contato</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </div>
     </div>
   );
